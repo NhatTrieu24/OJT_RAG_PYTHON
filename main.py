8d31a2e92a0c581420af458670b6266509198bde
NhatTrieu24 <nguyenhuunhattrieu2003@gmail.com>
2026-01-13 23:32:05 +0700

    add new url

--- main.py
+++ main.py
@@ -32,9 +32,14 @@ vertexai.init(project=PROJECT_ID, location=LOCATION)
 # ==================== CORS ====================
 app = FastAPI(title="RAG OJT 2025 – FINAL CLEAN", version="12.0")
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://frontend-ojt-544c.vercel.app", 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
